224fb9a6ee276d9972fbb304cb58cb67b9a35d0f
Naoman Saeed <naoman.saeed@gmail.com>
2026-05-11 08:41:51 +0500

    feat: serving static file conditionally.

--- src/server.js
+++ src/server.js
@@ -4,19 +4,32 @@ import fs from 'node:fs';
 const PORT = 3187;
 
 const server = http.createServer((req,res) => {
+    //---
     //const content = `<html><h1>The server is working.</h1></html>`;
     //res.statusCode = 200;
     //res.setHeader('Content-Type', 'text/html');
     //res.end(content);
-    fs.readFile('./public/index.html', (err, data) => {
-        if(err) {
-            res.statusCode = 500;
-            res.end('Error Loading Page.');
-            return;
-        }
-        res.setHeader('Content-Type', 'text/html');
-        res.end(data);
+    //----
+
+    // if the url is exactly '/', send index.html 
+    if (req.url === '/') {
+        fs.readFile('./public/index.html', (err, data) => {
+            if(err) {
+                // something happened on the way after the page was sent.
+                res.statusCode = 500;
+                res.end('Error Loading Page.');
+                return;
+            }
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
     });
+    } 
+    // otherwise, give 'not found' error.
+    else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('404: Page not found');
+    }
 });
 
 server.listen(PORT, () => console.log(`Server is listening at port: ${PORT}`));
