cbb97237e5e5ff08404b2bccffd33f84a1d853bf
Naoman Saeed <naoman.saeed@gmail.com>
2026-05-12 08:26:24 +0500

    feat: streamlined server logic.

---
+++ src/old_server.js
@@ -0,0 +1,61 @@
+import http from 'node:http';
+import fs from 'node:fs';
+
+const PORT = 3187;
+
+const server = http.createServer((req,res) => {
+    //---
+    //const content = `<html><h1>The server is working.</h1></html>`;
+    //res.statusCode = 200;
+    //res.setHeader('Content-Type', 'text/html');
+    //res.end(content);
+    //----
+
+    // if the url is exactly '/', send index.html 
+    if (req.url === '/') {
+        fs.readFile('./public/index.html', (err, data) => {
+            if(err) {
+                // something happened on the way after the page was sent.
+                res.statusCode = 500;
+                res.end('Error Loading Page.');
+                return;
+            }
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+    });
+    } 
+    // This code will run When CSS file is requested
+    else if (req.url === '/css/style.css') {
+        fs.readFile('./public/css/style.css', (err, data) => {
+            if (err) {
+                res.statusCode = 404;
+                res.end('CSS not found');
+                return;
+            }
+            res.setHeader('Content-Type', 'text/css');
+            res.end(data);
+        })
+    }
+    
+    //This code will run if app.js is called.
+    else if (req.url === '/js/app.js') {
+        fs.readFile('./public/js/app.js', (err, data) => {
+            if (err) {
+                res.statusCode = 404;
+                res.end('Script not found.');
+                return;
+            }
+            res.setHeader('Content-Type', 'application/javascript');
+            res.end(data);
+        })
+    }
+
+    // otherwise, give 'not found' error.
+    else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('404: Page not found');
+    }
+});
+
+server.listen(PORT, () => console.log(`Server is listening at port: ${PORT}`));
--- src/server.js
+++ src/server.js
@@ -3,58 +3,32 @@ import fs from 'node:fs';
 
 const PORT = 3187;
 
-const server = http.createServer((req,res) => {
-    //---
-    //const content = `<html><h1>The server is working.</h1></html>`;
-    //res.statusCode = 200;
-    //res.setHeader('Content-Type', 'text/html');
-    //res.end(content);
-    //----
+const server = http.createServer((req, res) => {
+    const getMimeType = (urlPath) => {
+        const ext = urlPath.split('.').pop().toLowerCase();
+        if (ext === 'css') {
+            return 'text/css';
+        }
+        else if (ext === 'js') {
+            return 'application/javascript';
+        }
+        else { return 'text/html'};
+    }
 
-    // if the url is exactly '/', send index.html 
     if (req.url === '/') {
         fs.readFile('./public/index.html', (err, data) => {
-            if(err) {
-                // something happened on the way after the page was sent.
-                res.statusCode = 500;
-                res.end('Error Loading Page.');
-                return;
-            }
-            res.setHeader('Content-Type', 'text/html');
-            res.end(data);
-    });
-    } 
-    // This code will run When CSS file is requested
-    else if (req.url === '/css/style.css') {
-        fs.readFile('./public/css/style.css', (err, data) => {
             if (err) {
                 res.statusCode = 404;
-                res.end('CSS not found');
-                return;
+                res.end('main file missing.');
+                return; 
             }
-            res.setHeader('Content-Type', 'text/css');
+            res.statusCode = 200;
+            res.setHeader('Content-Type', getMimeType(req.url));
             res.end(data);
-        })
-    }
-    
-    //This code will run if app.js is called.
-    else if (req.url === '/js/app.js') {
-        fs.readFile('./public/js/app.js', (err, data) => {
-            if (err) {
-                res.statusCode = 404;
-                res.end('Script not found.');
-                return;
-            }
-            res.setHeader('Content-Type', 'application/javascript');
-            res.end(data);
-        })
-    }
-
-    // otherwise, give 'not found' error.
-    else {
+        });
+    } else {
         res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('404: Page not found');
+        res.end('File Not Found.')
     }
 });
 
